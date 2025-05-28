550abde92b1e855c2f52cdcfba7d7b3af81657cd
Victor Lustosa <victor.olustosa@outlook.com>
2025-05-28 11:56:29 -0300

    refactor: updating securityConfig

--- src/main/java/br/com/unitins/censohgp/configs/SecurityConfig.java
+++ src/main/java/br/com/unitins/censohgp/configs/SecurityConfig.java
@@ -1,9 +1,9 @@
- package br.com.unitins.censohgp.configs;
+package br.com.unitins.censohgp.configs;
 
 import br.com.unitins.censohgp.security.JWTAuthenticationFilter;
 import br.com.unitins.censohgp.security.JWTAuthorizationFilter;
 import br.com.unitins.censohgp.utils.JWTUtil;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -25,55 +25,39 @@ import java.util.Arrays;
 
 @Configuration
 @EnableMethodSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    @Autowired
     private final UserDetailsService userDetailsService;
 
-    @Autowired
     private final JWTUtil jwtUtil;
-
-    public SecurityConfig(UserDetailsService userDetailsService, JWTUtil jwtUtil) {
-        this.userDetailsService = userDetailsService;
-        this.jwtUtil = jwtUtil;
-    }
 
     private static final String[] PUBLIC_MATCHERS = {};
 
     private static final String[] PUBLIC_MATCHERS_GET = {
             "/apicensohgp/paciente/**",
             "/apicensohgp/pacientes/**",
-            "/apicensohgp/precaucaos",
+            "/apicensohgp/precaucoes",
             "/apicensohgp/precaucao/**",
             "/apicensohgp/procedimentos/**"
     };
 
-    private static final String[] PUBLIC_MATCHERS_POST = {
-            "/auth/forgot/**"
-    };
+    private static final String[] PUBLIC_MATCHERS_POST = {"/auth/forgot/**"};
 
-    private static final String[] PUBLIC_MATCHERS_PUT = {
-    };
+    private static final String[] PUBLIC_MATCHERS_PUT = {};
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationManager authManager) throws Exception {
-        JWTAuthenticationFilter authFilter = new JWTAuthenticationFilter(authManager, jwtUtil);
-        JWTAuthorizationFilter authorizationFilter = new JWTAuthorizationFilter(authManager,jwtUtil, userDetailsService);
 
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .csrf(AbstractHttpConfigurer::disable)
-                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) // nova forma de desabilitar frameOptions
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                         .requestMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
                         .requestMatchers(HttpMethod.PUT, PUBLIC_MATCHERS_PUT).permitAll()
-                        .requestMatchers(PUBLIC_MATCHERS).permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(authFilter, UsernamePasswordAuthenticationFilter.class)
-                .addFilterBefore(authorizationFilter, UsernamePasswordAuthenticationFilter.class);
-
+                        .requestMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated())
+                .addFilter(new JWTAuthenticationFilter(authManager, jwtUtil))
+                .addFilterBefore(new JWTAuthorizationFilter(authManager, jwtUtil, userDetailsService),UsernamePasswordAuthenticationFilter.class);
+        http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
     }
 
